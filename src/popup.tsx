9fe0c24a345240d780c21cce02fb0d914c2e746f
henriquemsouza <henrique.m_s@hotmail.com>
2021-03-19 22:05:09 -0300

    chore:  add react-bootstrap  to project"

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 
 import React from "react";
 import styles from "./styles/styles";
+import { Button, Card, Container, Row } from "react-bootstrap";
 
 export class App extends React.Component<any, any> {
   constructor(props: any) {
@@ -24,17 +25,26 @@ export class App extends React.Component<any, any> {
   render() {
     return (
       <div style={styles.mainDiv}>
-        <span>{this.state.text}</span>
-
-        <button
-          onClick={() => {
-            this.getJoke().then((x) => {
-              this.setState({ text: x });
-            });
-          }}
-        >
-          Generate joke
-        </button>
+        <Container>
+          <Row>
+            <Card>
+              <Card.Body>{this.state.text}</Card.Body>
+            </Card>
+          </Row>
+          <Row>
+            <Button
+              variant="outline-primary"
+              size="lg"
+              onClick={() => {
+                this.getJoke().then((x) => {
+                  this.setState({ text: x });
+                });
+              }}
+            >
+              Generate joke
+            </Button>{' '}
+          </Row>
+        </Container>
       </div>
     );
   }
